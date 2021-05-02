9701fc175fdad27d53605c04342d5aed9c024b18
DDing <smg76262585@gmail.com>
2021-05-02 15:11:50 +0900

    헤더수정

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -106,50 +106,50 @@ DesktopContainer.propTypes = {
 }
 
 
-const ResponsiveContainer = ({ children }) => (
-  /* Heads up!
-   * For large applications it may not be best option to put all page into these containers at
-   * they will be rendered twice for SSR.
-   */
-  <MediaContextProvider>
-    <DesktopContainer>{children}</DesktopContainer>
-  </MediaContextProvider>
-)
+// const ResponsiveContainer = ({ children }) => (
+//   /* Heads up!
+//    * For large applications it may not be best option to put all page into these containers at
+//    * they will be rendered twice for SSR.
+//    */
+//   <MediaContextProvider>
+//     <DesktopContainer>{children}</DesktopContainer>
+//   </MediaContextProvider>
+// )
 
-ResponsiveContainer.propTypes = {
-  children: PropTypes.node,
-}
+// ResponsiveContainer.propTypes = {
+//   children: PropTypes.node,
+// }
 
-const HomepageLayout = () => (
-  <ResponsiveContainer>
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Grid container stackable verticalAlign='middle'>
-        <Grid.Row>
-          <Grid.Column width={8}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              We Help Companies and Companions
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              여기가
-            </p>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              We Make Bananas That Can Dance
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-                배너
-            </p>
-          </Grid.Column>
-          <Grid.Column floated='right' width={6}>
-            <Image bordered rounded size='large' src='/images/wireframe/white-image.png' />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column textAlign='center'>
-            <Button size='huge'>Check Them Out</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
+// const HomepageLayout = () => (
+//   <ResponsiveContainer>
+//     <Segment style={{ padding: '8em 0em' }} vertical>
+//       <Grid container stackable verticalAlign='middle'>
+//         <Grid.Row>
+//           <Grid.Column width={8}>
+//             <Header as='h3' style={{ fontSize: '2em' }}>
+//               We Help Companies and Companions
+//             </Header>
+//             <p style={{ fontSize: '1.33em' }}>
+//               여기가
+//             </p>
+//             <Header as='h3' style={{ fontSize: '2em' }}>
+//               We Make Bananas That Can Dance
+//             </Header>
+//             <p style={{ fontSize: '1.33em' }}>
+//                 배너
+//             </p>
+//           </Grid.Column>
+//           <Grid.Column floated='right' width={6}>
+//             <Image bordered rounded size='large' src='/images/wireframe/white-image.png' />
+//           </Grid.Column>
+//         </Grid.Row>
+//         <Grid.Row>
+//           <Grid.Column textAlign='center'>
+//             <Button size='huge'>Check Them Out</Button>
+//           </Grid.Column>
+//         </Grid.Row>
+//       </Grid>
+//     </Segment>
 
     {/* <Segment style={{ padding: '0em' }} vertical>
       <Grid celled='internally' columns='equal' stackable>
@@ -198,7 +198,7 @@ const HomepageLayout = () => (
       </Container>
     </Segment> */}
 
-  </ResponsiveContainer>
-)
+  {/* </ResponsiveContainer>
+) */}
 
-export default HomepageLayout
+export default DesktopContainer
