0563671e649a44215167e46f85920af4e281c1d6
husk <68003176+JangHanjun@users.noreply.github.com>
2021-05-10 16:22:20 +0900

    카트 기능 구현

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -3,8 +3,15 @@ import {
     LOGIN_USER,
     REGISTER_USER,
     AUTH_USER,
-    AUTH_ADMIN
+    AUTH_ADMIN,
+    ADD_TO_CART,
+    GET_CART_ITEMS,
+    REMOVE_CART_ITEM,
+    ON_SUCCESS_BUY
 } from './types';
+
+import { USER_SERVER } from '../Config';
+
 export function loginUser(dataToSubmit) {
 
     const request = axios.post('/api/users/login', dataToSubmit)
@@ -47,5 +54,77 @@ export function adminAuth(dagtTosubmit){
         type: AUTH_ADMIN,
         payload: request
 
+    }
+}
+
+export function addToCart(id) {
+    let body = {
+        productId: id
+    }
+    const request = axios.post(`${USER_SERVER}/addToCart`, body)
+        .then(response => response.data);
+
+    return {
+        type: ADD_TO_CART,
+        payload: request
+    }
+}
+
+export function getCartItems(cartItems, userCart) {
+
+    const request = axios.get(`/api/product/products_by_id?id=${cartItems}&type=array`)
+        .then(response => {
+            // CartItem들에 해당하는 정보들을  
+            // Product Collection에서 가져온후에 
+            // Quantity 정보를 넣어 준다.
+            userCart.forEach(cartItem => {
+                response.data.forEach((productDetail, index) => {
+                    if (cartItem.id === productDetail._id) {
+                        response.data[index].quantity = cartItem.quantity
+                    }
+                })
+            })
+            return response.data;
+        });
+
+    return {
+        type: GET_CART_ITEMS,
+        payload: request
+    }
+}
+
+
+export function removeCartItem(productId) {
+
+    const request = axios.get(`/api/users/removeFromCart?id=${productId}`)
+        .then(response => {
+            //productInfo ,  cart 정보를 조합해서   CartDetail을 만든다. 
+            response.data.cart.forEach(item => {
+                response.data.productInfo.forEach((product, index) => {
+                    if (item.id === product._id) {
+                        response.data.productInfo[index].quantity = item.quantity
+                    }
+
+                })
+            })
+            return response.data;
+        });
+
+    return {
+        type: REMOVE_CART_ITEM,
+        payload: request
+    }
+}
+
+
+
+export function onSuccessBuy(data) {
+
+    const request = axios.post(`/api/users/successBuy`, data)
+        .then(response => response.data);
+
+    return {
+        type: ON_SUCCESS_BUY,
+        payload: request
     }
 }
--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -2,7 +2,11 @@ import {
     LOGIN_USER,
     REGISTER_USER,
     AUTH_USER,
-    AUTH_ADMIN
+    AUTH_ADMIN,
+    ADD_TO_CART,
+    GET_CART_ITEMS,
+    REMOVE_CART_ITEM,
+    ON_SUCCESS_BUY
 } from '../_actions/types';
 
 export default function (state = {}, action) {
@@ -19,6 +23,31 @@ export default function (state = {}, action) {
         case AUTH_ADMIN:
             return { ...state, userData: action.payload}
             break;
+        case ADD_TO_CART:
+            return {
+                ...state,
+                userData: {
+                    ...state.userData,
+                    cart: action.payload
+                }
+            }
+        case GET_CART_ITEMS:
+            return { ...state, cartDetail: action.payload }
+        case REMOVE_CART_ITEM:
+            return {
+                ...state, cartDetail: action.payload.productInfo,
+                userData: {
+                    ...state.userData,
+                    cart: action.payload.cart
+                }
+            }
+        case ON_SUCCESS_BUY:
+            return {
+                ...state, cartDetail: action.payload.cartDetail,
+                userData: {
+                    ...state.userData, cart: action.payload.cart
+                }
+            }
         default:
             return state;
     }
