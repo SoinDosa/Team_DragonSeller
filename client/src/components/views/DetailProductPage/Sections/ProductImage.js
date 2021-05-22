919846eea4303506ba37f18f36b737b019aed874
SoinDosa <rlatjsgnl519@gmail.com>
2021-05-22 19:01:56 +0900

    Modify Product image

--- client/src/components/views/DetailProductPage/Sections/ProductImage.js
+++ client/src/components/views/DetailProductPage/Sections/ProductImage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import ImageGallery from 'react-image-gallery';
 
-function ProductImage() {
+const s3path = 'https://seonhwi.s3.amazonaws.com/';
+
+function ProductImage(props) {
+
+    const [Images, setImages] = useState([])
+
+    useEffect(() => {
+
+        if (props.detail.images && props.detail.images.length > 0) {
+            let images = []
+
+            props.detail.images.map(item => {
+                images.push({
+                    original: `${s3path}${item}`
+                })
+            })
+            setImages(images)
+        }
+
+    }, [props.detail])
+
     return (
         <div>
-            ProductImage
+            <ImageGallery items={Images} />
         </div>
     )
 }
