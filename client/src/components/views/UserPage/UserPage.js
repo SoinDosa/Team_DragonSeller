419feaee816428a1f21b916c6bb6813bd92446f4
DDing77 <smg76262585@gmail.com>
2021-05-23 02:04:38 +0900

    Add 개인페이지

---
+++ client/src/components/views/UserPage/UserPage.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios';
+import { withRouter } from 'react-router-dom';
+import Footer from '../Footer/Footer';
+import LogButton from '../Button/Button';
+import {
+    Button,
+    Container,
+    Divider,
+    Grid,
+    Icon,
+    Header,
+    Image,
+    List,
+    Menu,
+    Segment,
+    Sidebar,
+    Visibility,
+  } from 'semantic-ui-react'
+  import '../../../App.css';
+
+  function UserPage(props) {
+
+    const style = {
+        backgroundColor : '#1b1c1d',
+        width: '100vw',
+        height: '100vh'
+    }
+
+      return (
+        <div id= 'wrap' >
+            <div className= "headerWrap">
+              
+                {/* <div className="header logo">로고</div>
+                <div className="header header-name"> Dragon Seller </div>
+                <nav className="header Nav">
+                <LogButton/>
+                </nav> */}
+            </div>
+            <div id= 'containerWrap'>
+                <div id='main' style={style}>
+                    <Segment
+                    inverted
+                    textAlign='center'
+                    style={{ minHeight: '30px', padding: '2em 0em' }}
+                    vertical
+                    >
+                    <Container>
+                        <Header as='h1' color='red'>Dragon Seller</Header>
+                    </Container>
+                    <Container align="right"><LogButton/></Container>
+                    </Segment>
+                    <div style={{ width: '75%', margin: '3rem auto', }}>
+                        <h1 align="center" style={{color: 'white'}}>사용자 설정 메뉴</h1>
+                    </div>
+                    <div className="menuwrapper">
+                        <div className="menuwrapperitem">
+                            <br/>
+                            비밀번호 변경
+                            <form style={{ display: 'flex', flexDirection: 'column' }}>
+                                <br/>
+                                <div>
+                                    <Link to="changePw"><Button color='olive'>변경</Button></Link>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="menuwrapperitem"><br/>
+                            기본 주소지 설정
+                            <form style={{ display: 'flex', flexDirection: 'column' }}>
+                                <br/>
+                                <div>
+                                    <Button color='olive'>수정</Button>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="menuwrapperitem">
+                            <br/>
+                            장바구니 확인
+                            <form style={{ display: 'flex', flexDirection: 'column' }}>
+                                <br/>
+                                <div>
+                                    <Link to="/user/cart"><Button color='olive'>확인</Button></Link>
+                                </div>
+                            </form></div>
+                        <div className="menuwrapperitem"><br/>
+                            결제 내역
+                            <form style={{ display: 'flex', flexDirection: 'column' }}>
+                                <br/>
+                                <div>
+                                    <Link to='/product/upload'><Button color='olive'>확인</Button></Link>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          
+        </div>
+          
+      )
+  }
+
+  export default withRouter (UserPage)
