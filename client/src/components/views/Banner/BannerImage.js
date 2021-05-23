26901812fe91c80500bcbedbf26ebe5c3bc6700a
husk <68003176+JangHanjun@users.noreply.github.com>
2021-05-23 23:22:57 +0900

    배너 상세 페이지 구현

---
+++ client/src/components/views/Banner/BannerImage.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import ImageGallery from 'react-image-gallery';
+
+const s3path = 'https://seonhwi.s3.amazonaws.com/';
+
+function BannerImage(props) {
+
+    const [Images, setImages] = useState([])
+
+    useEffect(() => {
+
+        if (props.detail.images && props.detail.images.length > 0) {
+            let images = []
+
+            props.detail.images.map(item => {
+                images.push({
+                    original: `${s3path}${item}`
+                })
+            })
+            setImages(images)
+        }
+
+    }, [props.detail])
+
+    return (
+        <div>
+            <ImageGallery items={Images} />
+        </div>
+    )
+}
+
+export default BannerImage
