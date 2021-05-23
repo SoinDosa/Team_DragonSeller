f2588f8315a65aec69f6448830c56e31076a65a3
husk <68003176+JangHanjun@users.noreply.github.com>
2021-05-24 00:26:19 +0900

    상품 업로드 페이지 요구사항 충족

--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -17,6 +17,15 @@ function ProductInfo(props) {
         }
     }
 
+    const purchaseHandler = () => {
+        //필요한 정보를 Cart 필드에다가 넣어 준다.
+        if(Count <= 0)
+            alert("손님 갯수를 정해주셔야 합니다")
+        else {
+            alert("상품을 " + Count + "개 구매합니다!")
+        }
+    }
+
     const countChangeHandler = (event) => {
         setCount(event.currentTarget.value)
     }
@@ -33,6 +42,10 @@ function ProductInfo(props) {
                 <br/>
                 <Button type="primary" danger size="large"  htmlType="submit" onClick={clickHandler}>
                     카트 담기
+                </Button>
+                <br/>
+                <Button type="primary" danger size="large"  htmlType="submit" onClick={purchaseHandler}>
+                    바로 구매
                 </Button>
             </Form>
                 
