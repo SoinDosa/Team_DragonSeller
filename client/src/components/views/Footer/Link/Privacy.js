bef68fae37f7dcf0de56fca36b4575ece483b795
Haeil Lim <lhi7837@gmail.com>
2021-05-29 20:32:14 +0900

    footer
    fas

--- client/src/components/views/Footer/Link/Privacy.js
+++ client/src/components/views/Footer/Link/Privacy.js
@@ -2,6 +2,7 @@ import Header from '../../Header/Header'
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { Message } from 'semantic-ui-react'
 
 
 function Privacy(props) {
@@ -13,9 +14,11 @@ function Privacy(props) {
                 <Header />
             </div>
             <div>
+                <div > <h1>개인정보처리방침</h1> </div>
 
-                <div > <h1>프라이버시</h1> </div>
-
+                <Message>
+                    <Message.Header>DragonSeller는 이메일주소, 이름, 아이디, 비밀번호 외의 개인정보를 다루지 아니하며 해당 정보에 대해 잘 관리합니다.</Message.Header>
+                </Message>
             </div>
         </div>
     )
--- client/src/components/views/Footer/Link/SiteMap.js
+++ client/src/components/views/Footer/Link/SiteMap.js
@@ -1,7 +1,8 @@
 import Header from '../../Header/Header'
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
+import { Breadcrumb, List } from 'semantic-ui-react'
 
 
 function SiteMap(props) {
@@ -13,9 +14,39 @@ function SiteMap(props) {
                 <Header />
             </div>
             <div>
-
                 <div > <h1>사이트맵</h1> </div>
+                <List>
+                    <List.Item as='a'>
+                        <Breadcrumb>
+                            <Breadcrumb.Section link>
+                                <Link to="/">Home</Link>
+                            </Breadcrumb.Section>
 
+                        </Breadcrumb>
+                    </List.Item>
+                    <List.Item as='a'>
+                        <Breadcrumb>
+                            <Breadcrumb.Section link>
+                                <Link to="/">Home</Link>
+                            </Breadcrumb.Section>
+                            <Breadcrumb.Divider />
+                            <Breadcrumb.Section link>
+                                <Link to="/search">SHOP</Link>
+                            </Breadcrumb.Section>
+                        </Breadcrumb>
+                    </List.Item>
+                    <List.Item as='a'>
+                        <Breadcrumb>
+                            <Breadcrumb.Section link>
+                                <Link to="/">Home</Link>
+                            </Breadcrumb.Section>
+                            <Breadcrumb.Divider />
+                            <Breadcrumb.Section link>
+                                <Link to="/search">SHOP</Link>
+                            </Breadcrumb.Section>
+                        </Breadcrumb>
+                    </List.Item>
+                </List>
             </div>
         </div>
     )
